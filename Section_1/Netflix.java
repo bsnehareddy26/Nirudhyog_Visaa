b498ab2d11335a4a9c4387fcabb89e2e58e22aca
Sneha Reddy <157269457+bsnehareddy26@users.noreply.github.com>
2024-11-14 18:46:49 +0530

    Create Netflix.java

---
+++ Section_1/Netflix.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        int X = sc.nextInt();
+        
+        if((A+B) >= X){
+            System.out.println("YES");
+        }
+         else if ((B+C) >= X) {
+              System.out.println("YES");
+         }
+        else if ((A+C) >= X){
+           System.out.println("YES");
+        }
+        else {
+            System.out.println("NO");
+        }
+    }
+}
