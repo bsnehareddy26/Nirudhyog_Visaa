f7f4b50a5b0ecdabe89b93b00a14398f1a29ce40
Sneha Reddy <157269457+bsnehareddy26@users.noreply.github.com>
2024-11-14 18:45:04 +0530

    Create BridgeCrossing.java

---
+++ Section_1/BridgeCrossing.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        int Y = sc.nextInt();
+        int Z = sc.nextInt();
+        
+        Z -= Y;
+        System.out.println(Z/X);
+    }
+}
