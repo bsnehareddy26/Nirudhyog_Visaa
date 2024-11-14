c6a6839221107d1253fe656a560b080a324742b2
Sneha Reddy <157269457+bsnehareddy26@users.noreply.github.com>
2024-11-14 18:45:40 +0530

    Create CountNoteBooks.java

---
+++ Section_1/CountNoteBooks.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int pages = N * 1000;
+        int books = pages / 100;
+        System.out.println(books);
+    }
+}
